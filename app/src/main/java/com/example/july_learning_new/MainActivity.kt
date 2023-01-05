dbc459ec117fd3a75dacf51e98efc402d3545b20
Stepa075 <stepa0751@gmail.com>
2023-01-05 21:10:31 +0200

    with exemple of whether app

--- app/src/main/java/com/example/july_learning_new/MainActivity.kt
+++ app/src/main/java/com/example/july_learning_new/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.july_learning_new
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var example: TextView? = null
+    private var true_costs: TextView? = null
+    private var false_costs: TextView? = null
+
+    private var answer: Int? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        example = findViewById(R.id.example_info)
+        true_costs = findViewById(R.id.true_coast)
+        false_costs = findViewById(R.id.false_coast)
+
+//        example?.setText("This is example")
     }
 }
