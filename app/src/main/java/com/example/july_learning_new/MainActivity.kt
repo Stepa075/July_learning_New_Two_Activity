1be00bab3e63af9c951960316155b771d432c9e1
Stacionar-PC <stepa0751@gmail.com>
2023-01-06 08:40:47 +0200

    with registery all field in xml

--- app/src/main/java/com/example/july_learning_new/MainActivity.kt
+++ app/src/main/java/com/example/july_learning_new/MainActivity.kt
@@ -2,24 +2,36 @@ package com.example.july_learning_new
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     private var example: TextView? = null
     private var true_costs: TextView? = null
     private var false_costs: TextView? = null
+    private var main_btn: Button? = null
+    private var user_answer: EditText? = null
 
-    private var answer: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        main_btn = findViewById(R.id.main_btn)
         example = findViewById(R.id.example_info)
         true_costs = findViewById(R.id.true_coast)
         false_costs = findViewById(R.id.false_coast)
+        user_answer = findViewById(R.id.user_field)
+        main_btn?.setOnClickListener {
+            if (user_answer?.text?.toString()?.trim()?.equals("")!!)
+                Toast.makeText(this, "Введите число", Toast.LENGTH_LONG).show()
+            else {
+                val answer: String = user_answer?.text.toString()
+            }
+        }
 
-//        example?.setText("This is example")
     }
 }
