6a315733388d2480770625ddb7be8d41719620ea
Stepa075 <stepa0751@gmail.com>
2023-01-09 00:47:38 +0200

    First worked exempliar

--- app/src/main/java/com/example/july_learning_new/MainActivity.kt
+++ app/src/main/java/com/example/july_learning_new/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.july_learning_new
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,7 +16,15 @@ class MainActivity : AppCompatActivity() {
     private var false_costs: TextView? = null
     private var main_btn: Button? = null
     private var user_answer: EditText? = null
+    private var true_answer:String = ""
+    private var true_cos = 0
+    private var false_cos = 0
 
+    private var one = 0
+    private var two = 0
+    private var three = 0
+    private var znak1 = ""
+    private var znak2 = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,13 +35,65 @@ class MainActivity : AppCompatActivity() {
         true_costs = findViewById(R.id.true_coast)
         false_costs = findViewById(R.id.false_coast)
         user_answer = findViewById(R.id.user_field)
+
+        run_generate()
         main_btn?.setOnClickListener {
-            if (user_answer?.text?.toString()?.trim()?.equals("")!!)
+            if (user_answer?.text?.toString()?.trim()?.equals("")!!) {
                 Toast.makeText(this, "Введите число", Toast.LENGTH_LONG).show()
+            }
             else {
                 val answer: String = user_answer?.text.toString()
+                trueOrFalse(answer)
+
             }
         }
 
     }
+    @SuppressLint("SetTextI18n")
+    private fun run_generate(){
+        change_primer()
+
+        example?.text = "$one$znak1$two$znak2$three="
+
+    }
+
+
+    private fun trueOrFalse(value: String){
+                if(value == true_answer){
+                    true_cos++
+                    true_costs?.setText(true_cos.toString())
+        }
+        else{
+            false_cos++
+            false_costs?.setText(false_cos.toString())
+
+        }
+        run_generate()
+    }
+
+    private fun change_primer() {
+        var array = emptyArray<Int>()
+        while(true){
+
+            val random = Random()
+            var chislo1: Int = random.nextInt(100)
+            var chislo2: Int = random.nextInt(100)
+            var chislo3: Int = random.nextInt(100)
+            var variant: Int = random.nextInt(3)
+            var otvet: Int = 0
+            if(variant ==0){otvet = chislo1+chislo2+chislo3; znak1 = "+"; znak2 = "+"}
+            if(variant ==1){otvet = chislo1+chislo2-chislo3; znak1 = "+"; znak2 = "-"}
+            if(variant ==2){otvet = chislo1-chislo2+chislo3; znak1 = "-"; znak2 = "+"}
+            if(variant ==3){otvet = chislo1-chislo2-chislo3; znak1 = "-"; znak2 = "-"}
+            if(otvet >0){
+                true_answer = otvet.toString()
+                one = chislo1
+                two = chislo2
+                three = chislo3
+                break
+            }
+
+        }
+
+        }
 }
